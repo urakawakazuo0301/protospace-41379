c04cd7fbd8337b144d66179767ebcf3f584fb3bb
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-09 17:20:26 +0900

    createアクションの削除

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
 before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-before_action :move_to_index, only: [:new, :create, :edit, :update, :destroy]
+before_action :move_to_index, only: [:new, :edit, :update, :destroy]
 
 def index
   @prototypes = Prototype.all
